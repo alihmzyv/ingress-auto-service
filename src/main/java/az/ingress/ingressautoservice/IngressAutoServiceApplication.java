c1384c193f97fc53c6f1ce9450a74f1978ca66e9
Ali Hamzayev <alihmzyv@gmail.com>
2024-03-23 13:11:18 +0400

    add liquibase files.

---
+++ src/main/java/az/ingress/ingressautoservice/IngressAutoServiceApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.ingressautoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressAutoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressAutoServiceApplication.class, args);
+    }
+
+}
