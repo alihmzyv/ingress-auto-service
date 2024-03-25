8c8957c6a2ab3b4a1bc93025e55f371f30e71f4d
Ali Hamzayev <alihmzyv@gmail.com>
2024-03-25 15:50:02 +0400

    add todo.

--- src/main/java/az/ingress/ingressautoservice/entity/helper/CarDetails.java
+++ src/main/java/az/ingress/ingressautoservice/entity/helper/CarDetails.java
@@ -15,6 +15,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Embeddable
 public class CarDetails {
+    //TODO: add Brand as field
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     Model model;
